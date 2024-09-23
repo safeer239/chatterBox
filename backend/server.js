157c935b4dfadeb256397c283940676cbd01fa35
safeer239 <mohammedsafeershefi@gamil.com>
2024-09-23 15:54:48 +0530

    fixex error

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,13 @@ app.use("/api/user", userRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/message", messageRoute);
 
-const _dirname=path.resolve()
+// const __dirname=path.resolve()
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(_dirname,"/frontend/dist")))
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname,"/frontend/dist/index.html"))
+    res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"))
   })
 }
 
