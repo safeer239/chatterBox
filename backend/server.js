cf3754ca4cac0a6eeccf666731d88898e9484ffe
safeer239 <mohammedsafeershefi@gamil.com>
2024-09-23 16:21:50 +0530

    fixed production

--- backend/server.js
+++ backend/server.js
@@ -14,22 +14,24 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is available");
-});
+
 
 app.use("/api/user", userRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/message", messageRoute);
 
-// const __dirname=path.resolve()
+const __dirname1=path.resolve()
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  app.use(express.static(path.join(__dirname1,"/frontend/dist")))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"))
+    res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
   })
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is available");
+  });
 }
 
 app.use(errorHandler);
